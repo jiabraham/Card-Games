247b494c9a5df6034095e5488a3cb80adac64aca
Joseph I. Abraham <jia253@nyu.edu>
2019-08-01 15:23:00 -0400

    Update cards.py

--- cards.py
+++ cards.py
@@ -3,13 +3,18 @@ import math
 import random
 import player
 
-class Player:
-    def __init__(self, money, name, status):
-        self.money = money
-        self.hand = {}
+class card:
+    def __init__(self, name, classification):
         self.name = name
-        self.status = 0
-
+        self.classification = classification
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        self.name = self.name + name
+    def getClassification(self, classification):
+        return self.classification
+    def setClassification(self, classification):
+        self.classification = self.classification
 #Function to create and set dictionary to hold cards
 def setDeck():
     deck = {}
@@ -17,24 +22,24 @@ def setDeck():
         #Set card values
         value = i % 13;
         if (value > 0 and value < 10):
-            deck[i] = str(value+1)
+            deck[i] = card(str(value+1), value+1)
         if (value == 0):
-            deck[i] = "Ace"
+            deck[i] = card("Ace", 14)
         if (value == 10):
-            deck[i] = "Jack"
+            deck[i] = card("Jack", 11)
         if (value == 11):
-            deck[i] = "Queen"
+            deck[i] = card("Queen", 12)
         if (value == 12):
-            deck[i] = "King"
+            deck[i] = card("King", 13)
         #Set card suits
         if (i > -1 and i < 13):
-            deck[i]+= " of Spades"
+            deck[i].setName(" of Spades")
         if (i > 12 and i < 26):
-            deck[i]+= " of Hearts"
+            deck[i].setName(" of Hearts")
         if (i > 25 and i < 39):
-            deck[i]+= " of Clubs"
+            deck[i].setName(" of Clubs")
         if (i > 38 and i < 52):
-            deck[i]+= " of Diamonds"
+            deck[i].setName(" of Diamonds")
     return deck
 
 #Function to create and set cards_dealt vector
@@ -90,12 +95,24 @@ def handRanking(player_vector, pool):
 
 
         player_vector_index = player_vector_index + 1
-            return "winning hand is"
+    return "winning hand is"
+
+#UNFINISHED
+def straightFlush(hand_vec):
+    #given flush
+    #if normal flush return 0
+    #if straight flush return 1
+    #if royal flush return 2
+    return 0
+
 #UNFINISHED
 #Function to return a hand ranking 1-10
 #May have to edit later to include high card to differentiate same ranking
 def classifyHand(hand_vec):
     #First we should sort the hand based on values(bubble-sort of 7 elements is negligible)
+    hand_vec = sortHand(hand_vec)
+
+    #Check for flush
     k = 0
     hand_vec_length = len(hand_vec)
     flush_count = {}
@@ -104,6 +121,8 @@ def classifyHand(hand_vec):
     flush_count["clubs"] = 0
     flush_count["diamonds"] = 0
     flush = False
+
+    #Loop once through hand to count the number of each suit
     while (k < hand_vec_length):
         if (hand_vec[k][-6:] == "Spades"):
             flush_count["spades"] = flush_count["spades"] + 1
@@ -121,8 +140,23 @@ def classifyHand(hand_vec):
             flush_count["diamonds"] = flush_count["diamonds"] + 1
             if (flush_count["diamonds"] > 4):
                 flush  = True
+    #Must calculate the highest flush either here or in handRanking
+    if (flush):
+        return 6
+        #flush_type = straightflush(hand_vec)
+
+    #Check for straight(double loop)
+    
+    for i in range(0, 3):
+        for j in range(i, i+5):
+            if (hand_vec[j+1].getClassification() == hand_vec[j].getClassification() + 1)
 
 def sortHand(hand_vec):
+    #Quicksort later if this is causing a time issue
     for i in range(0,7):
-        for j in range(1,7):
-            if ((hand_vec[i][:3]) == "Ace")
+        for j in range(0,7):
+            if (hand_vec[j].classification < hand_vec[i].classification):
+                temp = hand_vec[i]
+                hand_vec[i] = hand_vec[j]
+                hand_vec[j] = temp
+    return hand_vec

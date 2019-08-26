7d5d9eefe962933590600278fd1a3c665d3167cd
Joseph I. Abraham <jia253@nyu.edu>
2019-08-26 08:43:40 -0400

    Update cards.py

--- black_jack/cards.py
+++ black_jack/cards.py
@@ -1,1 +1,67 @@
+#!/usr/bin/python3
+import math
+import player
+import random
+
+
+class card:
+    def __init__(self, name, classification):
+        self.name = name
+        self.classification = classification
+    def getName(self):
+        return self.name
+    def setName(self, name):
+        self.name = self.name + name
+    def getClassification(self):
+        return self.classification
+    def setClassification(self, classification):
+        self.classification = self.classification
+
+#Function to create and set dictionary to hold cards
+def setDeck():
+    deck = {}
+    for i in range (0, 52):
+        #Set card values
+        value = i % 13;
+        if (value > 0 and value < 10):
+            deck[i] = card(str(value+1), value+1)
+        if (value == 0):
+            deck[i] = card("Ace", 1)
+        if (value == 10):
+            deck[i] = card("Jack", 10)
+        if (value == 11):
+            deck[i] = card("Queen", 10)
+        if (value == 12):
+            deck[i] = card("King", 10)
+        #Set card suits
+        if (i > -1 and i < 13):
+            deck[i].setName(" of Spades")
+        if (i > 12 and i < 26):
+            deck[i].setName(" of Hearts")
+        if (i > 25 and i < 39):
+            deck[i].setName(" of Clubs")
+        if (i > 38 and i < 52):
+            deck[i].setName(" of Diamonds")
+    return deck
+
+#Function to create and set cards_dealt vector
+def setCardsDealt():
+    cards_dealt = {}
+    for i in range (0, 52):
+        cards_dealt[i] = 0
+    return cards_dealt
+
+#Function to draw a random card
+def draw(deck, cards_dealt):
+    deck_index = random.randint(0,51)
+    #check if random index is the same as one that's already in use
+    if (cards_dealt[deck_index] == 0):
+        cards_dealt[deck_index] = 1
+        return deck[deck_index]
+    return draw(deck, cards_dealt)
+
+def deal(player_number):
+    of
+
+def hit():
 
